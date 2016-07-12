00c05a08b532961df569f8c95d2b8b527be2a3c3
zithroooo <zithroooo@gmail.com>
2016-07-12 13:48:21 +0600

    rual-11716

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Linkable BD Map </title>
+    <script src="master.js" charset="utf-8"></script>
+  </head>
+  <body>
+    <?php
+    include 'image.php';
+      ?>
+  </body>
+</html>
---
+++ php/master.js
@@ -0,0 +1,49 @@
+function rangpur() {
+   window.open("#");
+}
+function rajshahi() {
+  window.open("#");
+}
+function dhaka() {
+  window.open("#");
+}
+function khulna() {
+  window.open("#");
+}
+function barisal() {
+  window.open("#");
+}
+function sylhet() {
+  window.open("#");
+}
+function chittagong() {
+  window.open("http://facebook.com");
+}
+function rual(text) {
+  switch (text) {
+    case "rangpur":
+         rangpur();
+        break;
+    case "rajshahi":
+         rajshahi();
+        break;
+    case "dhaka":
+         dhaka();
+        break;
+    case "khulna":
+         khulna();
+        break;
+    case "barisal":
+        barisal();
+        break;
+    case "sylhet":
+        sylhet();
+        break;
+    case "chittagong":
+        chittagong();
+}
+}
+
+function over(text) {
+  document.getElementById(text).style.fill = '#419B2E';
+}
